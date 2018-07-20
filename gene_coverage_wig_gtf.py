09b1eb384f45bfc5b5cd480764e41f2e68570dfa
Diane Trout <diane@ghic.org>
2018-07-20 15:14:07 -0700

    Adjust order of arguments to group them better

--- gene_coverage_wig_gtf.py
+++ gene_coverage_wig_gtf.py
@@ -69,23 +69,24 @@ def main(cmdline=None):
 
 def make_parser():
     parser = ArgumentParser()
+    parser.add_argument('--gtf', required=True, help='GTF file name')
+    parser.add_argument('wig', help='wiggle file to score')
+    parser.add_argument('-o', '--output', required=True, help='output file name')
     parser.add_argument('--source-type', help='source type name')
-    parser.add_argument('--normalize', action='store_true', default=False,
-                        help='normalize scores')
     parser.add_argument('--max-gene-length', default=None, type=int,
                         help='maximum gene length to consider')
-    parser-add_argument('--all-gene-models', default=False, action='store_true',
+    parser.add_argument('--min-gene-length', type=int, default=1000,
+                        help='minimum gene length to consider')
+    parser.add_argument('--all-gene-models', default=False, action='store_true',
                         help='use all gene models, not just the ones with a single model')
     parser.add_argument('--gene-type', help='limit to specified gene type')
     parser.add_argument('--print-list', default=False, action='store_true',
                         help='write gene ids considered to <outfile>.geneList')
+    parser.add_argument('--normalize', action='store_true', default=False,
+                        help='normalize scores')
     parser.add_argument('-q', '--quiet', action='store_true',
                         help='only report errors')
-    parser.add_argument('--gtf', help='GTF file name')
-    parser.add_argument('wig', help='wiggle file to score')
-    parser.add_argument('--min-gene-length', type=int, default=1000,
-                        help='minimum gene length to consider')
-    parser.add_argument('output-filename', help='output file name')
+    parser.add_argument('--version', action='store_true', help='report version number')
     return parser
 
 def readAnnotation(stream, source, doSingleModel, gene_type_filter=None):
