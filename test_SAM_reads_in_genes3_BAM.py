24554f6555bfe77750bef307221f77ec3e69ce14
Diane Trout <diane@ghic.org>
2018-09-18 13:51:51 -0700

    PEP8: Wrap long lines to 80 columns

--- test_SAM_reads_in_genes3_BAM.py
+++ test_SAM_reads_in_genes3_BAM.py
@@ -30,39 +30,48 @@ class SAMReadsInGenesBAMTest(unittest.TestCase):
         intronicReads = 0
         posCountsDict = {}
         geneDict = {}
-        self.assertEqual((exonicReads, intronicReads), getCounts(posCountsDict, geneDict, verbose=False))
+        self.assertEqual((exonicReads, intronicReads),
+                         getCounts(posCountsDict, geneDict, verbose=False))
 
     def testGetCountsSingle(self):
         geneDict = {"chr1": {"gene1": set([(1, 5), (15, 30)])}}
         exonicReads = 1
         intronicReads = 0
         posCountsDict = {"chr1": {20: 1}}
-        self.assertEqual((exonicReads, intronicReads), getCounts(posCountsDict, geneDict, verbose=False))
+        self.assertEqual((exonicReads, intronicReads),
+                         getCounts(posCountsDict, geneDict, verbose=False))
         exonicReads = 0
         intronicReads = 1
         posCountsDict = {"chr1": {10: 1}}
-        self.assertEqual((exonicReads, intronicReads), getCounts(posCountsDict, geneDict, verbose=False))
+        self.assertEqual((exonicReads, intronicReads),
+                         getCounts(posCountsDict, geneDict, verbose=False))
         exonicReads = 0
         intronicReads = 0
         posCountsDict = {"chr1": {40: 1}}
-        self.assertEqual((exonicReads, intronicReads), getCounts(posCountsDict, geneDict, verbose=False))
+        self.assertEqual((exonicReads, intronicReads),
+                         getCounts(posCountsDict, geneDict, verbose=False))
 
     def testGetCountsMulti(self):
         geneDict = {"chr1": {"gene1": [(1, 5), (15, 30)]}}
         exonicReads = 3
         intronicReads = 4
         posCountsDict = {"chr1": {3: 2, 10: 4, 20: 1, 40: 7}}
-        self.assertEqual((exonicReads, intronicReads), getCounts(posCountsDict, geneDict, verbose=False))
+        self.assertEqual((exonicReads, intronicReads),
+                         getCounts(posCountsDict, geneDict, verbose=False))
 
     def testReadGtfEmpty(self):
         gtf = []
         self.assertEquals({}, readGtf(gtf))
 
     def testReadGtfMulti(self):
-        gtf = ['chr2\t\texon\t0\t10\t\t+\t\tgene_id "gene1";transcript_id "tr1";',
-               'chr2\t\texon\t20\t50\t\t+\t\tgene_id "gene1";transcript_id "tr1";',
-               'chr2\t\texon\t0\t10\t\t+\t\tgene_id "gene1";transcript_id "tr2";',
-               'chr3\t\texon\t0\t80\t\t-\t\tgene_id "gene2";transcript_id "tr1";'
+        gtf = ['chr2\t\texon\t0\t10\t\t+\t\t'
+               'gene_id "gene1";transcript_id "tr1";',
+               'chr2\t\texon\t20\t50\t\t+\t\t'
+               'gene_id "gene1";transcript_id "tr1";',
+               'chr2\t\texon\t0\t10\t\t+\t\t'
+               'gene_id "gene1";transcript_id "tr2";',
+               'chr3\t\texon\t0\t80\t\t-\t\t'
+               'gene_id "gene2";transcript_id "tr1";'
                ]
         geneDict = {'chr2': {'gene1': set([(0, 10), (20, 50)])},
                     'chr3': {'gene2': set([(0, 80)])}
